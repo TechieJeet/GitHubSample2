ef7e6249065a3fb59ca9d03f0f0e066e4dded0a7
ranjeet <ramjeetp.mum@gmail.com>
2025-11-04 10:45:58 +0530

    New Framework Created

---
+++ GitHubSample2/src/main/java/GitHubSample2/GitHubSample2/App.java
@@ -0,0 +1,13 @@
+package GitHubSample2.GitHubSample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
